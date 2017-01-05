2bc41d7b5139c8693340f01f26040947579deff9
Kyle Macey <kylemacey@github.com>
2017-01-05 16:29:30 -0500

    Test to ensure that timestamp provided is a valid Date
    Signed-off-by: Briana Swift <brianamarie@github.com>

--- tests/test_date.rb
+++ tests/test_date.rb
@@ -2,15 +2,10 @@ require 'test/unit'
 require "yaml"
 
 class TestHTMLValidation < Test::Unit::TestCase
-
   def test_htmlvalidation
     Dir["./_pages/*.yaml"].each do |path|
       f = YAML.load_file(path)
-      [
-        f["timestamp"],
-      ].each do |s|
-        assert_not_nil(s.to_s[/(201\d{1}-(0[1-9]|1[0-2])-[0-3]{1}\d{1})/], path)
-      end
+      assert_true(f["timestamp"].is_a?(Date), "Invalid Date format '#{f["timestamp"]}' in #{path}")
     end
   end
 end
