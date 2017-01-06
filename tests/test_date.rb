eb61b859220a05747f93a5503b6bc4ca8cda6275
Briana Swift <brianamarie@github.com>
2017-01-06 09:51:38 -0600

    change assert_true to assert

--- tests/test_date.rb
+++ tests/test_date.rb
@@ -5,7 +5,7 @@ class TestHTMLValidation < Test::Unit::TestCase
   def test_htmlvalidation
     Dir["./_pages/*.yaml"].each do |path|
       f = YAML.load_file(path)
-      assert_true(f["timestamp"].is_a?(Date), "Invalid Date format '#{f["timestamp"]}' in #{path}")
+      assert(f["timestamp"].is_a?(Date), "Invalid Date format '#{f["timestamp"]}' in #{path}")
     end
   end
 end
