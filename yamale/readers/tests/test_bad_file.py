3828d19110334abc013d8ec3438f4ad21c69395d
Tien Quang Nguyen <tien-q-nguyen2@gmail.com>
2018-09-26 10:05:41 -0600

    test
    test

--- yamale/readers/tests/test_bad_file.py
+++
@@ -1,7 +0,0 @@
-from pytest import raises
-from .. import parse_file
-
-
-def test_reader_error():
-    with raises(IOError):
-        parse_file('wat')
--- yamale/schema/__init__.py
+++
@@ -1,2 +0,0 @@
-from .schema import Schema
-from .data import Data
